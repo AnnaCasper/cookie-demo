e07a76a6089f92de967dacfde658e5781b476d5f
Anna Casper <anna.casper3@gmail.com>
2015-06-24 11:50:54 -0600

    added user chosen background color

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var currentUser = req.cookies.currentUser
+  var currentUser = req.cookies.currentUser;
+  var backgroundColor = req.cookies.color;
   var visits = parseInt(req.cookies.visits, 10) || 0;
-  visits ++
-  res.cookie('visits', visits);
-  res.render('index', { title: 'Cookie Demo', currentUser: currentUser, visits: visits });
+    visits ++
+    res.cookie('visits', visits);
+  res.render('index', {
+    title: 'Cookie Demo',
+    currentUser: currentUser,
+    visits: visits,
+    backgroundColor: backgroundColor
+    });
 });
 
 router.post('/fake-login', function(req, res, next){
@@ -18,6 +24,12 @@ router.post('/fake-login', function(req, res, next){
 router.post('/logout', function(req, res, next){
   res.clearCookie('currentUser');
   res.clearCookie('visits');
+  res.clearCookie('color');
+  res.redirect('/');
+});
+
+router.post('/color', function(req, res, next){
+  res.cookie('color', req.body.color);
   res.redirect('/');
 });
 
