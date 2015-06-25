9a51c40e2111111849d25d99c4d819e2fb753f94
Anna Casper <anna.casper3@gmail.com>
2015-06-25 13:01:45 -0600

    successful sign up and login page

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+var db = require('monk')('process.env.MONGO_URI');
+var cookieCollection = db.get('cookies');
+
+var bcrypt = require('bcryptjs');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var currentUser = req.cookies.currentUser;
   var backgroundColor = req.cookies.color;
+  var currentLoginUser = req.cookies.currentLoginUser
+  var error = req.query.error;
   var visits = parseInt(req.cookies.visits, 10) || 0;
     visits ++
     res.cookie('visits', visits);
@@ -12,19 +18,37 @@ router.get('/', function(req, res, next) {
     title: 'Cookie Demo',
     currentUser: currentUser,
     visits: visits,
-    backgroundColor: backgroundColor
+    backgroundColor: backgroundColor,
+    currentLoginUser: currentLoginUser,
+    error: error,
     });
 });
 
-router.post('/fake-login', function(req, res, next){
+router.post('/signup', function(req, res, next){
   res.cookie('currentUser', req.body.user_name);
+  var hash = bcrypt.hashSync(req.body.password, 8);
+  cookieCollection.insert({ username: req.body.user_name, password: hash});
   res.redirect('/');
+});
+
+router.post('/login', function(req, res, next){
+  cookieCollection.findOne({ username: req.body.login_user_name}, function(err, data){
+    console.log('hello');
+    var compare = bcrypt.compareSync(req.body.login_password, data.password);
+    if (compare === true) {
+      res.cookie('currentLoginUser', req.body.login_user_name);
+      res.redirect('/');
+    } else {
+      res.redirect('/?error=BOO!!');
+    };
+  });
 });
 
 router.post('/logout', function(req, res, next){
   res.clearCookie('currentUser');
   res.clearCookie('visits');
   res.clearCookie('color');
+  res.clearCookie('currentLoginUser');
   res.redirect('/');
 });
 
